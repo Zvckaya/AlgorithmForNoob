9fd2508db736745237a045130ad3d2064a2dcc82
lzvckaya <lzvckaya@gmail.com>
2023-06-30 20:56:54 +0900

    sol rotation of the sequence

---
+++ Array and LinkedList/rotation of the sequence.py
@@ -0,0 +1,9 @@
+from collections import deque
+
+def solution(nums,k):
+    answer = deque(nums)
+    for i in range(k):
+        answer.append(answer.popleft())
+    return list(answer)
+
+print(solution([3,7,1,5,9,2,8],3))
